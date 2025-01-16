ddcc81160ae495ca51fe426249eb0933fc5ed168
code1218 <skjil1218@gmail.com>
2025-01-16 17:51:33 +0900

    useEffect

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import App5 from './study/App5';
 import App6 from './study/App6';
 import App7 from './study/App7';
 import App8 from './study/App8';
+import App9 from './study/App9';
+import App10 from './study/App10';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,6 +19,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App4 />);
 // root.render(<App5 />);
 // root.render(<App7 />);
-root.render(<App8 />);
+// root.render(<App8 />);
+// root.render(<App9 />);
+root.render(<App10 />);
 
 
